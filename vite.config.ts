659e43d1024192599733e36ed37c87cd00e2e1ce
Timon Schmidt <timon.schmidt@netlight.com>
2025-03-23 01:09:48 +0100

    jekyll yourself

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? "/timonschmidt-github.io/" : "/",
+  base: "/",
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
@@ -13,10 +14,18 @@ export default defineConfig(({ mode }) => ({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   },
-  plugins: [
-    react(),
-  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
